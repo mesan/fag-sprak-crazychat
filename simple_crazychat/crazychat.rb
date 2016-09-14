ce782f7beec0df74ab8af16dbc66503865081f19
Daniel Rødskog <danielr@mesan.no>
2016-09-14 20:33:34 +0200

    Ta imot input og sende meldinger
    Funker ikke helt enda -- har noe problemer med tråder og greier.

--- simple_crazychat/crazychat.rb
+++ simple_crazychat/crazychat.rb
@@ -1,37 +1,78 @@
 #!/usr/bin/env ruby
 
-$stderr = File.new("stderr.log", 'w')
-
 require 'sinatra/base'
 require 'sinatra/reloader'
 require 'json'
-require 'logger'
+require 'rest-client'
+
+my_address, my_port, my_name = ARGV
+raise "Oppgi addresse, portnummer og navn!" unless my_address && my_port && my_name 
 
 class CrazyChat
-  def initialize
-    @users = {}
+  def initialize(address, port, name, users={})
+    @address = address
+    @name = name
+    @users = users
+  end
+
+  def add_user(username, address)
+    @users[username] = address
+    p @users
   end
 
   def handle_message(body)
     username = body['username']
     message = body['message']
-    address = body['address']
+    address = body['returnAddress']
 
-    @users[username] = address
+    add_user(username, address)
 
     puts "<#{username}> #{message}"
   end
+
+  def send_message(message)
+    puts "SEND!"
+    @users.each do |name, address|
+      puts "Sender til #{name} (#{address})..."
+      RestClient.post "http://#{address}", {"message" => message,
+                                            "username" => @name,
+                                            "returnAddress" => "http://#@address:#@port"}
+      exit
+
+    end
+  end
 end
+
+chat_client = CrazyChat.new(my_address, my_port.to_i, my_name)
 
 class MyApp < Sinatra::Base
   register Sinatra::Reloader
 
-  post '/chat' do
+  post '/' do
     message = JSON.parse(request.body.read)
-    settings.chat.handle_message(message)
+    settings.chat_client.handle_message(message)
     200
   end
 end
 
-MyApp.set :chat, CrazyChat.new
-MyApp.run!
+$stderr = File.new("stderr-#$$.log", 'w')
+
+MyApp.set :chat_client, chat_client
+MyApp.set :port, my_port.to_i
+server_thread = Thread.new { MyApp.run! }
+
+Thread.new do
+  loop do
+    print "> "
+    input = $stdin.gets.chomp
+
+    case input
+    when /^:add (\w+) (.+)/
+      chat_client.add_user($1, $2)
+    else
+      chat_client.send_message(input)
+    end
+  end
+end
+
+server_thread.join
