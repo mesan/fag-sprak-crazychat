784ccedff4dca478cd4c603a52485435d6a40a37
philipbhs <philiph@mesan.no>
2016-09-27 18:38:07 +0200

    send og motta meldinger

--- src/main/java/ChatClient.java
+++ src/main/java/ChatClient.java
@@ -1,22 +1,48 @@
+import org.apache.http.HttpEntity;
+import org.apache.http.client.HttpClient;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 import rx.subjects.PublishSubject;
 import spark.Request;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.apache.http.client.methods.*;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 import static spark.Spark.*;
 
 public class ChatClient {
 
-    ArrayList<String> receivers = new ArrayList<String>();
+    private CopyOnWriteArrayList<String> receivers = new CopyOnWriteArrayList<>();
+    private String address;
+    private String name;
+    private HttpClient client;
 
-    public ChatClient(){
+    public ChatClient(String[] args){
+        client = HttpClientBuilder.create().build();
+        try {
+            address = "http://" + Inet4Address.getLocalHost().getHostAddress() + ":4567/hello";
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        name = args[0];
+        for (int i = 1; i < args.length; i++){
+            receivers.add(args[i]);
+        }
         PublishSubject<Request> subject = PublishSubject.create();
         subject.subscribe(this::handle_msg);
         post("/hello", (req,res) -> {subject.onNext(req); return "soiudjflkdsf";});
+
+        startClient();
     }
     public void handle_msg(Request req){
         JSONObject jsonObject = null;
@@ -43,7 +69,39 @@ public class ChatClient {
         Scanner scanner = new Scanner(System.in);
         while (true){
             String input = scanner.next();
-            System.out.println("melding inn: "+input+"\n");
+//            System.out.println("melding inn: "+input+"\n");
+
+            sendMessage(formatMessage(input));
+        }
+    }
+
+    private JSONObject formatMessage(String input){
+        JSONObject obj = new JSONObject();
+        obj.put("message", input);
+        obj.put("username", name);
+        obj.put("returnAddress", address);
+
+        return obj;
+    }
+
+    private void sendMessage(JSONObject obj) {
+        StringEntity body = null;
+
+        try {
+            body = new StringEntity(obj.toJSONString());
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        for (String rec: receivers){
+            System.out.println(rec);
+            HttpPost pm = new HttpPost(rec);
+            pm.setEntity(body);
+            try {
+                client.execute(pm);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- src/main/java/CrazyChat.java
+++ src/main/java/CrazyChat.java
@@ -5,6 +5,7 @@
  */
 public class CrazyChat {
     public static void main(String[] args){
-        new ChatClient();
+
+        new ChatClient(args);
     }
 }
